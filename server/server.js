e1a3cdb424da63f8af02b7de34523cdf75dd1a3e
Yilin Xu <yx296@nyu.edu>
2015-10-16 19:15:55 -0700

    Initialize server and UserModel/Controller

--- server/server.js
+++ server/server.js
@@ -1,3 +1,55 @@
+var express = require('express');
+var parser = require('body-parser');
+var mongoose = require('mongoose');
+
+var app = express();
+
+var server = require('http').createServer(app);
+
+var socketHandler = require(__dirname + '/socketHandler.js');
+var routes = require(__dirname + '/routes.js');
+var users = require(__dirname + 'features/users/users.js');
+var auth = require(__dirname + 'features//auth.js');
+
+app.use(parser.json());
+app.use(express.static(__dirname + '/../client'));
+app.use('/', routes);
+// app.use('/auth', auth);
+// app.use('/users', users);
+
+server.listen(process.env.PORT || 1337);
+
+module.exports.server = server;
+
+console.log("Express server listening");
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('ERROR!');
+  err.status = 404;
+  next(err);
+});
+
+
+mongoose.connect('mongodb://localhost/locket');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
   serve static files ../client
   
@@ -6,7 +58,4 @@
 
   route calls through: 
     routes.js:
-    handler["users"].get
-
-
 */
