dc4e3c064f8c6398a9aa70bd7ec3f569dbe1d3cc
Michael Wetherald <michaelwetherald@gmail.com>
2015-10-17 16:01:51 -0700

    Implement basic messaging and friends list

--- client/app/features/chat/chat.js
+++ client/app/features/chat/chat.js
@@ -1,24 +1,131 @@
 angular.module('Locket.chat', [])
 
 .controller('chatController', function ($scope) {
-  
+  var socket = io();
+
   $scope.friends = [{
     service: "facebook",
     username: "nate",
     name: "nate dawg",
     newMessage: false,
+    online: true,
     messages: [{
       to: 'nate',
       from: 'me',
       message: 'hi friend!',
       timestamp: new Date()
     }]
+  },
+  {
+    service: "facebook",
+    username: "livvie",
+    name: "livvie dawg",
+    newMessage: false,
+    online: true,
+    messages: [{
+      to: 'livvie',
+      from: 'me',
+      message: 'hi livvie!',
+      timestamp: new Date()
+    }]
   }];
 
   //currently hardcoded for first friend
+  //represents the user selected in the friends list
   $scope.activeFriend = $scope.friends[0];
 
+  $scope.startChat = function(friend){
+    findFriend(friend, function(index){
+      $scope.activeFriend = $scope.friends[index];
+    });
+
+    //if $scope.friends[username] has publicPGPKey
+      //update chat view with current conversation
+    //else
+      //if $scope.friends[username].service is us
+        //render messages in chat view
+      //else if username.service isnt us
+        //allow unencrypted chat
+        //show red encryption symbol/button (warning user chat is not secure)
+  };
+
+
+  //messaging
+  $scope.sendMessage = function(messageText){
+    console.log($scope.activeFriend, messageText);
+    //reset message text
+    $scope.messageText = '';
+    socket.emit('sendMessage', { to: $scope.activeFriend, message: messageText });
+    //if service is us
+      //if we have recipients pgp key
+        //encrypt and send message
+      //else
+        //notify user you are requesting recipient's pgp key
+        //socket.emit('requestKey', {to: recipient})
+    //else
+      //request other user's public key and save message contents to be encrypted on receipt of user's key 
+        //user would like to have an encrypted conversation with you:
+        //user's public key
+  };
+
+  socket.on('newMessage', function(message){
+
+    findFriend(message.from, function(index){
+      $scope.apply(function(){
+        $scope.friends[index].messages.push(message);
+      });
+    });
+
+  });
+
+  //friends
+  $scope.addFriend = function(username){
+    socket.emit('addFriend', { to: username });
+  };
+
+  socket.on('friendLoggedIn', function(friend){
+    findFriend(friend, function(index){
+      //if user is in friends list
+      if(index >= 0){
+        $scope.friends[index].online = true;
+      }else{
+        //if user is not in friends list, add them
+        $scope.friends.push(friend);
+      }
+    });
+  });
+  
+  socket.on('friendLoggedOut', function(friend){
+    findFriend(friend, function(index){
+      //verify user is in friends list
+      if(index >= 0){
+        $scope.friends[index].online = false;
+      }
+    });
+  });
+
+  socket.on('friendRequest', function(friendRequest){
+    
+    console.log('friend request recieved from ' + friendRequest);
+  });
+
+
+  //hoist helper functions
+  function findFriend(friend, cb){
+    for(var i =0; i < $scope.friends.length; i++){
+      if($scope.friends[i].username === friend.username){
+        cb(i);
+        return;
+      }
+    }
+
+    //if friend not in list
+    cb(-1);
+  }
+
 });
+
+
 
 /*
 
@@ -51,7 +158,7 @@ angular.module('Locket.chat', [])
         //update chat view with current conversation
       //else
         //if $scope.friends[username].service is us
-          
+          //render messages in chat view
         //else if username.service isnt us
           //allow unencrypted chat
           //show red encryption symbol/button (warning user chat is not secure)
@@ -91,7 +198,7 @@ angular.module('Locket.chat', [])
       socket.emit('sendPGP') {key: pgpKey, to: from}
     
     //friends
-    socket.on('friends') update friends list
+    socket.on('refreshFriends') update friends list when user comes online or goes offline
     emit new friends socket.emit('addFriend', {to: })  
     
     intervalled (it's a word now) requests to content script for new messages and friends
