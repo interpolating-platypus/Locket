c3aff4c449e09d60431ad5b7842f93c60916a72b
Nate Willard <nate.a.willard@gmail.com>
2015-10-19 19:35:29 -0700

    remove unnecessary proof of concept message sending code

--- extension/proof-of-concept/facebook.js
+++ extension/proof-of-concept/facebook.js
@@ -4,14 +4,6 @@ var backgroundCheckInterval = 1000;
 
 $(document).ready(function() {
 
-  // var test = $('#webMessengerRecentMessages').text();
-  // console.log(test);
-  // document.getElementsByName('message_body')[0].value = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaah panic';
-  // document.getElementById('u_0_r').click();
-  // chrome.runtime.sendMessage({
-  //   data: test
-  // });
-
   var checkWithBackgroundProcess = function() {
     chrome.runtime.sendMessage({event: 'updateStatus' }, 
       function(response) {
