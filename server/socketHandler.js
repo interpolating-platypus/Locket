966a3f6e27e746894ef068eb19b7c6d3f8ac1506
Nate Willard <nate.a.willard@gmail.com>
2015-10-21 12:00:08 -0700

    add comment to explain echo fn

--- server/socketHandler.js
+++ server/socketHandler.js
@@ -130,6 +130,7 @@ io.on('connection', function(socket) {
     console.log('disconnect usermap', userMap);
   });
 
+  // Echo function, useful for debugging & testing
   socket.on('echo', function(obj) {
     socket.emit(obj.event, obj.data);
   });
