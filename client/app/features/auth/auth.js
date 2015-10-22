861336b32200c39bcadc60dd9d9bd38fa504a875
Livvie Lin <livvie.lin@gmail.com>
2015-10-21 19:07:43 -0700

    Fix login and signup error messages

--- client/app/features/auth/auth.js
+++ client/app/features/auth/auth.js
@@ -5,17 +5,21 @@ angular.module('Locket.auth', [])
   $scope.signupFailed = false;
 
   $scope.login = function(){
-    authFactory.login($scope.user.usernameLogin, $scope.user.passwordLogin);
+    authFactory.login($scope.user.usernameLogin, $scope.user.passwordLogin)
+    .catch(function () {
+      $scope.loginFailed = true;
+    });
     $scope.user.usernameLogin = '';
     $scope.user.passwordLogin = '';
-    $scope.loginFailed = true;
   };
 
   $scope.signup = function(){
-    authFactory.signup($scope.user.usernameSignup, $scope.user.passwordSignup);
+    authFactory.signup($scope.user.usernameSignup, $scope.user.passwordSignup)
+    .catch(function () {
+      $scope.signupFailed = true;
+    });
     $scope.user.usernameSignup = '';
     $scope.user.passwordSignup = '';
-    $scope.signupFailed = true;
   };
 
 });
