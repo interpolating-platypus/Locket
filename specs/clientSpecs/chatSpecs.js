5fef890496431ddf84f18fc4f7a06e9c3e8cce18
Livvie Lin <livvie.lin@gmail.com>
2015-10-23 16:58:24 -0700

    Revert to old chatSpecs

--- specs/clientSpecs/chatSpecs.js
+++ specs/clientSpecs/chatSpecs.js
@@ -1,69 +1,22 @@
 var testTimeout = 1000;
 var testDuration = 2000;
 describe("chat tests", function(){
-  var mockEncryptionFactory = {
-    generateKeyPair: function () {
-      var keyring = {};
-      var options = {
-        numBits: 2048,
-        userId: 'randomId',
-        passphrase: 'randomPassphrase'
-      };
-      
-      return openpgp.generateKeyPair(options)
-      .then(function (keypair) {
-        keyring.privkey = keypair.privateKeyArmored;
-        keyring.pubkey = keypair.publicKeyArmored;
-      }).then(function () {
-        return keyring;
-      }).catch(function (error) {
-        // failure
-      });
-      // return {
-      //   pubkey: '-----BEGIN PGP PUBLIC KEY BLOCK 12345 END PGP PUBLIC KEY BLOCK-----',
-      //   privkey: '-----BEGIN PGP PRIVATE KEY BLOCK abcde END PGP PRIVATE KEY BLOCK-----'
-      // };
-    },
-    encryptMessage: function (keyring, message) {
-      // return message + keyring;
-      var pubKey = keyring.pubkey;
-      return openpgp.encryptMessage(openpgp.key.readArmored(pubKey).keys, message)
-      .then(function (pgpMessage) {
-        // console.log('encrypted message sent', pgpMessage);
-        return pgpMessage;
-      });
-    },
-    decryptMessage: function (keyring, pgpMessage) {
-      // return pgpMessage - keyring;
-      var privKey = keyring.privkey;
-      var privateKey = openpgp.key.readArmored(privKey).keys[0];
-      privateKey.decrypt(options.passphrase);
-      pgpMessage = openpgp.message.readArmored(pgpMessage);
-
-      return openpgp.decryptMessage(privateKey, pgpMessage).then(function (plaintext) {
-        // console.log('decrypted message', plaintext);
-        return plaintext;
-      });
-    }
-  };
-  beforeEach(module(function($provide) {
-    $provide.value('encryptionFactory', mockEncryptionFactory);
-  }));
   beforeEach(module('Locket'));
   beforeEach(module(function($urlRouterProvider) {
     $urlRouterProvider.deferIntercept();
   }));
-  var $controller, socket;
-
-  var authFactory = {};
-  // var encryptionFactory = {};
+  var $controller, authFactory, socket;
 
   // Inject a fresh controller
-  beforeEach(inject(function(_$controller_, _socket_, _authFactory_){
+  beforeEach(inject(function(_$controller_, _socket_){
     $controller = _$controller_;
     socket = _socket_;
+  }));
+  
+  // Inject auth factory
+  var authFactory = {};
+  beforeEach(inject(function(_authFactory_) {
     authFactory = _authFactory_;
-    // encryptionFactory = _encryptionFactory_;
   }));
 
   describe('Chat controller', function() {
@@ -116,11 +69,6 @@ describe("chat tests", function(){
         done();
       });
       $scope.activeFriend = {username: 'nate', service: "Locket"};
-      mockEncryptionFactory.generateKeyPair().then(function () {console.log('mock encryption factory')});
-      $scope.activeFriend = {
-        username: 'nate',
-        key: '-----BEGIN PGP PUBLIC KEY BLOCK-----Version: OpenPGP.js v1.3.0Comment: http://openpgpjs.org xsBNBFYqupoBCAD67+4RzDQ9SjlUrPMc+gDxUikTWmfhIJZL0/C/odHa00yW+YyQ8Qf9m6ZXE7vrbGTcUHev5CVtYac2PiwakjoCTw/ZgMXidYjEFlcAJ9CHe9Y5BobsOj89zd11P8wgSM5ZeBCNU7fqT+od7++prQs57ydJdXacW92S+n5DHIbpIbtGb8+sKmyXG+n5Zs1C7YPWfJ8t13TC5SB/lsesxIyt62h/f6xzpsnLEEj7oEtr2OquHm3FSXtLsi6PmBcx8uLk1iJaqiuDNOyNhywPrbzrSp2SKXxqPcNWF5JlVrK1UHLIOFREHJhIUTHASKV3LzZD6FZchYkHAe2Y/9paqAOVABEBAAHNCHJhbmRvbUlkwsByBBABCAAmBQJWKrqbBgsJCAcDAgkQYfy0So7sTM0EFQgCCgMWAgECGwMCHgEAAF0ACAC8eCFjIMnuLlGaNE7GuHOLjLqftu3DDZY/5F9wA6SuGFYr4rvWGOKrve5rHpWErnRWa+PzEKYzFCd85eGC/9dks7P1Gctf2Rp8tCavx+B3Qs7ZaecmMIw9GZKCU0wARSRC5QMAX0xuv9bmiSqF/ZnQcmVJ9W+19ztjlUc9gTaPR6JvAJrn4eznDChM12d+x/4cCxlkEQ5O7HYVOpzNuAt5a/gJxth2jim0byjvfg80vztKUwOz4esE05cRJI4NafKbkr1EX/0y3an8vs/HSZbc92uJf1ccrvwwC2cPR/Huz40S+TdbbnjgnkjA6dV0F2+/0hkvHxqer7BjHk9ygVV7zsBNBFYqupoBCADCLDgzlDaf4j9MTCRMu1QKUf8DESHhpR/oGm+mFuVRnd7SNIdi49nKaYj/bJ8VqzxcMlyOsX6RNgIJy0SnUDwHykuzv0agKVURUf5zG5iBVhH3UgbUerGpGwuGUjrRUgjjSZDQKAdWhiRUDqcNlV0EitYA4QPTpOLct4yopUW5SZZgX2VYoarC3biamfUHHoqmu+f0qbYa0BLsVKApURvvMsVY7W9+f7bGBG/kUj2qAuPeWRwHcOzWJ8MP4JMgigwog4revuNzJxWnX/wa41m/6p7XPuADuGNnQLkKHp3mE7EfjJhtM9aOlqLZTmOdk990nLA6EeSBDq7sSroPiPZnABEBAAHCwF8EGAEIABMFAlYqupsJEGH8tEqO7EzNAhsMAAAB+ggA5y7BXCJepl2zTlUzwlPg3g0l+MbEOvdsUBALdS+VDELsPVQwFhJm63nSGhtZUr7E8Co2CKf3FlvfldDjKn+qQ9bfPRK/zXuHLvCGPXKEPILnrNZrV+q+niX+griBgwGOI69ovNkwted6ExfxVmrszIKzRa6Ww51sUq7dTJZGIb1QdUlUFeXz6kraaikIp82CuZFRItsqSoUsSvOBn/lkSsbp74e5PdTIT3GQKE9LGoNoQUsX8gKNwu9/jUKH7iSGOuKMD9Ed1vSMPzVKVu5V1++u+F4xjJZtpiofi6iJ4F0xaqk+SxXXCkTeYnoRJc/PMpnkh4oyatjKVn3hDQ8Y7w===2v6K-----END PGP PUBLIC KEY BLOCK-----'
-      };
       $scope.sendMessage('hi');
       socket.emit.restore();
     });
