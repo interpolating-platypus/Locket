8d620fde2a9f4782d9777c2ed0435e7735088bb5
Michael Wetherald <michaelwetherald@gmail.com>
2015-10-30 18:15:56 -0700

    find friend's chat window

--- extension/proof-of-concept/hangouts.js
+++ extension/proof-of-concept/hangouts.js
@@ -11,12 +11,14 @@ var elementIdentifiers = {
   //ignore headers in friends list of this type
   friendsListUsernameIgnoreClass: 'sV',
   //username that appears at top of chat
-  chatWindowRecipient: '.Ob2Lud'
+  chatWindowRecipient: '.Ob2Lud',
+  //class of iframe containing chat window
+  chatWindowIframeClass: '.talk_chat_widget'
 }
 
-$(document).ready(function(){
+$(document).ready(function () {
   
-  var getFrameDepth = function(winToID) {
+  var getFrameDepth = function (winToID) {
     if (winToID === window.top) {
       return 0;
     } else if (winToID.parent === window.top) {
@@ -32,20 +34,20 @@ $(document).ready(function(){
 
   console.log('hangouts');
 
-  setInterval(function(){
+  setInterval(function () {
     getHangoutsFriends();
-    findFriendChat("Jose Barrientos");
+    console.log(findFriendMessages("Jose Barrientos"));
   }, rescanDOMInteral);
 
 });
 
-var getHangoutsFriends = function() {
+var getHangoutsFriends = function () {
 
   //friends list is stored in an iframe
   var friendObjs = $(elementIdentifiers.friendsListIframeId).contents().find(elementIdentifiers.friendsListIframeButton);
   var friends = [];
 
-  friendObjs.each(function(){
+  friendObjs.each(function () {
     var headers = $(this).find(elementIdentifiers.friendsListUsernameType);
     if(!$(headers[0]).hasClass(elementIdentifiers.friendsListUsernameIgnoreClass)){
       var name = $(headers[0]).text();
@@ -65,12 +67,19 @@ var getHangoutsFriends = function() {
 
 };
 
-var findFriendChat = function(name){
-  // console.log(userToButtonMap[name].button);
-  $('.talk_chat_widget').each(function(){
-    var recipient = $(this).find('iframe').contents().find(elementIdentifiers.chatWindowRecipient).text();
-    if (recipient) {
-      console.log(recipient);
+var findFriendChatWindow = function (name) {
+  var friendChatWindow = null;
+  $(elementIdentifiers.chatWindowIframeClass).each(function () {
+    var chatWindow = $(this).find('iframe');
+    var recipient = chatWindow.contents().find(elementIdentifiers.chatWindowRecipient).text();
+    if (recipient === name) {
+      friendChatWindow = chatWindow;
     }
   });
+
+  return friendChatWindow;
 };
+
+var findFriendMessages = function () {
+
+}
