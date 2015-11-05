fd09aea879c3537cae1ab604d8e9e86740e6aea5
Yilin Xu <yx296@nyu.edu>
2015-11-05 15:11:47 -0800

    Clean up server file

--- server/server.js
+++ server/server.js
@@ -2,7 +2,6 @@ var express = require('express');
 var parser = require('body-parser');
 var mongoose = require('mongoose');
 
-// var mongodb = require('mongodb');
 var uriUtil = require('mongodb-uri');
 
 var options = { server: { socketOptions: { keepAlive: 1, connectTimeoutMS: 30000 } }, 
@@ -29,13 +28,7 @@ var socketSession = exports.socketSession = require('express-socket.io-session')
 var app = express();
 
 var server = require('http').createServer(app);
-// mongoose.connect('mongodb://localhost/locket');
 
-
-
-// var routes = require(__dirname + '/routes.js');
-// var users = require(__dirname + 'features/users/users.js');
-// var auth = require(__dirname + 'features/auth.js');
 var userRouter = express.Router();
 
 app.use(parser.json());
@@ -46,15 +39,6 @@ app.use(passport.session());
 
 app.use(express.static(__dirname + '/../client'));
 app.use('/api/users', userRouter);
-// app.use('/auth', auth);
-// app.use('/users', users);
-
-// catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('ERROR!');
-//   err.status = 404;
-//   next(err);
-// });
 
 
 server.listen(process.env.PORT || 1337);
