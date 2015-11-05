9ea159b1987ba5274ebfba06565c848cf83211f7
Nate Willard <nate.a.willard@gmail.com>
2015-11-05 14:46:08 -0800

    Facebook.js cleanup

--- extension/facebook.js
+++ extension/facebook.js
@@ -1,4 +1,3 @@
-console.log('facebook');
 var backgroundCheckInterval = 500;
 var rescanDOMInterval = 500;
 var seenMessageGroup = {};
@@ -10,9 +9,7 @@ var scanDOM = true;
 var maxFacebookFriends = 15;
 
 $(document).ready(function() {
-  // Facebook has iFrames. 
-  // We only want to execute our code on OUR iFrame (the parent frame)
-  // Retrieve the frame depth
+  // Retrieve the depth of the iFram
   var getFrameDepth = function(winToID) {
     if (winToID === window.top) {
       return 0;
@@ -21,9 +18,9 @@ $(document).ready(function() {
     }
     return 1 + getFrameDepth (winToID.parent);
   }
+
   // Only inject code into the outermost iFrame
   if (getFrameDepth(window.self) === 1) {
-    console.log('facebook iFrame');
 
     // Retrieves list of most-recently talked-with facebook friends
     var getFacebookFriends = function() {
@@ -43,7 +40,7 @@ $(document).ready(function() {
       return results;
     };
 
-    // Check for mew messages from background process
+    // Checks for mew messages from background process
     var checkWithBackgroundProcess = function() {
       chrome.runtime.sendMessage({event: 'updateStatus' }, 
         function(response) {
@@ -52,24 +49,25 @@ $(document).ready(function() {
             for (var i = 0; i < response.postMessages.length; i++) {
               var message = response.postMessages[i];
 
-              // Store the messages to be sent
+              // Stores the messages to be sent
               messagesToPost[message.to] = messagesToPost[message.to] || [];
               messagesToPost[message.to].push(message.text);
 
-              // Store users to navigate to in order to send messages
+              // Stores users to navigate to in order to send messages
               if (usersToClick.indexOf(message.to) === -1) {
                 usersToClick.push(message.to);
               }
             }
           }
 
-          // Background process wants us to initiate a key exchange. Same logic as handling posting of messages
+          // Background process wants us to initiate a key exchange
           if (response.sendPublicKey.length !== 0) {
             for (var i = 0; i < response.sendPublicKey.length; i++) {
               var keyReq = response.sendPublicKey[i];
 
-              // Mark that we want to initiate a key exchange with this user
-              keyExchanges[keyReq.to] = keyReq.publicKey + "*****Your Key Below******" + keyReq.friendKey + "END KEYSHARE";//keyReq.friendKey is what the sending user thinks the recipient's key is
+              // Marks that we want to initiate a key exchange with this user
+              // keyReq.friendKey is what the sending user thinks the recipient's key is
+              keyExchanges[keyReq.to] = keyReq.publicKey + "*****Your Key Below******" + keyReq.friendKey + "END KEYSHARE";
               if (usersToClick.indexOf(keyReq.to) === -1) {
                 usersToClick.push(keyReq.to);
               }
@@ -78,9 +76,9 @@ $(document).ready(function() {
 
           // Background process wants to get facebook friends
           if (response.getFriends) {
-            // Retrieve friends from facebook DOM
+            // Retrieves friends from facebook DOM
             var friends = getFacebookFriends();
-            // Send to the background process
+            // Sends these friends to the background process
             chrome.runtime.sendMessage({
               event: 'facebookFriendsList',
               data: friends
@@ -102,7 +100,6 @@ $(document).ready(function() {
               messagesToPost[username] = messagesToPost[username] || [];
               messagesToPost[username].push("*****USER DISCONNECT*****");
             }
-            console.log('emitting disconnect messages', usersToClick, messagesToPost);
           }
 
           // Background process wants us to change status of DOM monitoring
@@ -111,27 +108,28 @@ $(document).ready(function() {
       );
     }
 
-    // Check for new messages from facebook DOM
+    // Checks for new messages from facebook DOM
     var rescanFacebookDom = function() {
-      // Check for new messages from the active user
+      // Checks for new messages from the active user
       $('#webMessengerRecentMessages li').each(function(idx) {
         var texts = $(this).find('p');
         var id = $(this).attr('id');
         seenMessageGroup[id] = seenMessageGroup[id] || [];
 
-        // Handle any new messages
+        // Handles any new messages
         if (id && seenMessageGroup[id].length !== texts.length) {
           var newTexts = [];
-          var partialPGPKey = ''; // the pgp key gets split into two messages
+          // The pgp key is split into multiple messages
+          var partialPGPKey = ''; 
           var context = this;
           texts.slice(seenMessageGroup[id].length).each(function(index) {
-            // Sometimes a new message will contain a PGP key
             if (partialPGPKey) {
               var text = $(this).text();
               partialPGPKey += '\n\n' + text;
               if (text.slice(text.length-12) === 'END KEYSHARE') {
               
-                var pgpKeys = partialPGPKey.split('*****Your Key Below******');//index 0 = sender's key, index 1 = what the sender has stored as recipient's key
+                var pgpKeys = partialPGPKey.split('*****Your Key Below******');
+                //index 0 = sender's key, index 1 = what the sender has stored as recipient's key
                 var friendKey = pgpKeys[1].substring(0, pgpKeys[1].length-12);
                 var activeUsername = getActiveUsername();
                 var activeName = getActiveName();
@@ -142,7 +140,8 @@ $(document).ready(function() {
                     event: 'receivedPGPKey',
                     data: {
                       publicKey: pgpKeys[0],
-                      friendKey: friendKey,//this is what the friend thinks this user's current key is
+                      // friendKey is what the friend thinks this user's current key is
+                      friendKey: friendKey,
                       from: activeUsername,
                       name: activeName
                     }
@@ -164,39 +163,36 @@ $(document).ready(function() {
           });
 
           if (newTexts.length) {
-            // Retrieve the facebook username
+            // Retrieves the facebook username
             var activeUsername = getActiveUsername();
             var activeName = getActiveName();
             var sentBy = getSender(this, activeUsername);
 
-            // handle sending of new messages to the client here
+            // Handles sending of new messages to the client here
             chrome.runtime.sendMessage({event: 'receivedNewFacebookMessage', data: {with: activeUsername, name: activeName, from: sentBy, text: newTexts}});
           }
           seenMessageGroup[id] = texts;
         }
       });
 
-      // Send any queued messages / key exchanges for this user
+      // Sends any queued messages / key exchanges for this user
       var activeUsername = getActiveUsername();
-        //$('._r7').find('a').attr('href').replace('https://www.facebook.com/','');
-
-      // Send any queued key exchanges to this user
+      // Sends any queued key exchanges to this user
       if (keyExchanges[activeUsername] && keyExchanges[activeUsername].length) {
         var message = keyExchanges[activeUsername];
         postFacebookMessage(message);
         delete keyExchanges[activeUsername];
       }
 
-      // Send any queued messages to this user
+      // Sends any queued messages to this user
       if (messagesToPost[activeUsername] && messagesToPost[activeUsername].length !== 0) {
-        // In a while loop in case more messages are received while we are sending messages
         while (messagesToPost[activeUsername].length) {
           var message = messagesToPost[activeUsername].shift();
           postFacebookMessage(message);
         }
       }
 
-      // Check for new messages from non-active users
+      // Checks for new messages from non-active users
       usersWithNewMessages = [];
       $('._k_').each(function(idx) {
         var username = $(this).attr('href').replace('https://www.facebook.com/messages/','');
@@ -204,28 +200,32 @@ $(document).ready(function() {
           usersWithNewMessages.push({username: username, link: this});
         }
       });
+
       // If there are any users with new messages, add them as to-be-clicked-uponn
       if (usersWithNewMessages.length !== 0) {
         if (usersToClick.indexOf(usersWithNewMessages[0].username) === -1) {
           usersToClick.push(usersWithNewMessages[0].username);
         }
       }
-      // Navigate to the non-active user with a new message
+
+      // Navigates to the non-active user with a new message
       if (usersToClick.length !== 0) {
-        // Retrieve the link for that username
+        // Retrieves the link for that username
         var usernameToClick = usersToClick.shift();
-        // var usernameToClick = usersToClick[0];
         $('._k_').each(function() {
           var username = $(this).attr('href').replace('https://www.facebook.com/messages/','');
           if (username === usernameToClick) {
-            // Click it
+            // Clicks the username
             this.click();
           }
         });
       }
     };
 
+    // Periodically checks with the background process for new instructions
     setInterval(checkWithBackgroundProcess, backgroundCheckInterval);
+
+    // Periodically checks whether it's no longer in use and should be turned off
     setInterval(function() {
       if (scanDOM || usersToClick.length || messagesToPost.length) {
         rescanFacebookDom();
@@ -235,18 +235,24 @@ $(document).ready(function() {
     }, rescanDOMInterval);
   }
 
+  // Posts a message on Facebook through the DOM
   function postFacebookMessage(message) {
     document.getElementsByName('message_body')[0].value = message;
     $('input[value="Reply"]').click();
-    //document.getElementById('u_0_r').click();
   }
+
+  // Retrieves the username of a sender of a message
   function getSender(context, activeUsername) {
     return $(context).find('a').first().attr('href').replace('https://www.facebook.com/','').replace('profile.php?id=','') === activeUsername ? activeUsername : 'me';
   }
+
+  // Retrieves the username of the person being talked to
   function getActiveUsername() {
     var activeUsername = $('._r7').find('a').attr('href').replace('https://www.facebook.com/','').replace('profile.php?id=','');
     return activeUsername;
   }
+
+  // Returns the name of the person being talked to
   function getActiveName() {
     return $('._r7').find('a').text();
   }
